77957f508be3412ad94cd229227bb7b93fd83f10
Doug McNish <git@stopavoiding.us>
2016-07-12 13:36:06 -0400

    Wrapper scripts

---
+++ jot.bat
@@ -0,0 +1,5 @@
+REM This is just an example batch script one could use to set defaults.
+REM Replace i386 with amd64 if you want to use 64 bit exe.
+REM This might even work.
+
+jot-windows-i386.exe -note=%*
---
+++ jot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Example of a bash wrapper that can be used to set command line options.
+
+
+note="$@"
+
+$GOPATH/bin/jot -note="$note"
