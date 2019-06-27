b6470bd5d8159c5c6b1e70b0c3b8c737747cb916
Doug McNish <git@stopavoiding.us>
2019-06-27 15:01:45 -0400

    Added support for newlines in note body

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ import (
 	"time"
 )
 
-func prepend(fname string, note string, timestamp string) error {
+func prepend(fname string, note string, timestamp string, newline bool) error {
 
 	buf, _ := ioutil.ReadFile(fname)
+  format := "%v%v\n\n%v"
+
+	if newline {
+		format = "%v\n%v\n\n%v"
+	}
 
 	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	defer f.Close()
@@ -20,14 +25,21 @@ func prepend(fname string, note string, timestamp string) error {
 		return err
 	}
 
-	if _, err := f.WriteString(fmt.Sprintf("%v%v\n\n%v", timestamp, note, string(buf))); err != nil {
+	if _, err := f.WriteString(fmt.Sprintf(format, timestamp, note, string(buf))); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func append(fname string, note string, timestamp string) error {
+func append(fname string, note string, timestamp string, newline bool) error {
+
+	format := "%v%v\n\n"
+
+	if newline {
+		format = "%v\n%v\n\n"
+	}
+
 	f, err := os.OpenFile(fname, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 
 	if err != nil {
@@ -36,7 +48,7 @@ func append(fname string, note string, timestamp string) error {
 
 	defer f.Close()
 
-	if _, err := f.WriteString(fmt.Sprintf("%v%v\n\n", timestamp, note)); err != nil {
+	if _, err := f.WriteString(fmt.Sprintf(format, timestamp, note)); err != nil {
 		return err
 	}
 
@@ -54,6 +66,7 @@ func main() {
 	note := flag.String("note", "Tick..", "Note content")
 	datefmt := flag.String("datefmt", "Jan-2006", "Date Format - Changing the date format can change how often a file is rotated. (see golang time package)")
 	timefmt := flag.String("timefmt", "**Mon, Jan 2 15:04:05:** ", "Timestamp Format (see golang time package)")
+  newline := flag.Bool("newline", false, "Print a newline after datetime stamp.")
 
 	flag.Parse()
 
@@ -64,12 +77,12 @@ func main() {
 	notefile := path.Join(*notepath, fmt.Sprintf("%v%v%v", *prefix, date, *suffix))
 
 	if *pre {
-		if err := prepend(notefile, *note, timestamp); err != nil {
+		if err := prepend(notefile, *note, timestamp, *newline); err != nil {
 			panic(err)
 		}
 
 	} else {
-		if err := append(notefile, *note, timestamp); err != nil {
+		if err := append(notefile, *note, timestamp, *newline); err != nil {
 			panic(err)
 		}
 	}
